6a702a58ef45cf78108ce0fa6b98c72f49b0b3bc
Leonardo <workmail.leo69@gmail.com>
2023-12-19 14:54:44 -0300

    New Actions v1
    - Attack action
    - Regen action

--- player/actions.lua
+++ player/actions.lua
@@ -1,7 +1,51 @@
 local actions = {}
 
-function actions.build(playerData)
+actions.list = {}
 
+function actions.build()
+    actions.list = {}
+
+    local mageAttack = {
+        description = "",
+        requeriment = nil,
+
+        execute = function (playerData, creatureData)
+            -- Chances de Sucesso
+            local sucessChance = creatureData.speed == 0 and 1 or (playerData.speed / creatureData.speed)
+            local sucess = math.random() <= sucessChance
+
+            -- Calcula o dano
+            local rawDamage = playerData.attack - (math.random() * creatureData.defense)
+            -- Dano final
+            local damage = math.max(1, math.ceil(rawDamage))
+
+            if sucess then
+                print(string.format("Voce causou %d de dano a criatura!", damage))
+
+                -- Aplica o dano a criatura
+                creatureData.health = creatureData.health - damage
+            else
+                print("Voce errou o ataque!")
+            end
+        end
+    }
+
+    local potions = {
+        description = "",
+        requeriment = function (playerData, creatureData)
+            return playerData.potions >= 1 
+        end,
+
+        execute = function (playerData, creatureData)
+            -- Remove pocao
+            playerData.potions = playerData.potions - 1
+
+
+            -- Recupera vida
+            local regen = 5
+            playerData.health = math.min(playerData.MAXHEALTH, (playerData.health + regen))
+        end
+    }
 end
 
 function actions.getValidActions()
--- player/player.lua
+++ player/player.lua
@@ -10,6 +10,7 @@ player.health = 10
 player.attack = 4
 player.defense = 2
 player.speed = 3
+player.potions = 3
 player.Type = nil
 
 return player
